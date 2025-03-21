df57006e297112c568f5c03ef5326f50b2cd8722
Victor <victor.vj1612@hotmail.com>
2025-03-21 19:56:43 -0300

    mensagem

---
+++ Main1.py
@@ -0,0 +1,17 @@
+# O (nome) é a variável que guarda um valor ou dado
+# O (=) significa que recebe
+# O input chama uma função 
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+curso = input('Digite seu curso: ')
+
+print('Seu nome é: ',nome)
+print('Sua idade é:',idade)
+print('Seu curso é: ',curso)
+# (print) chama a função de mostrar na tela
+# ('') é uma string, serve para guardar caracteres, alfanumericos ou simbolos
+# Declarei 3 variáveis nome,idade e curso
+# Chamei a função (input)
+# Chamei a função (string) para guardar um dado de palavras
+# Chamei a função (print) 
+# Chamei a função (string) novamente para guardar outro dado de palavras, depois adicionei as variáveis em cada um dos seus respectivos lugares
