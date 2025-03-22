01f6f728c956876f0280523ea64d6179074de9eb
Victor <victor.vj1612@hotmail.com>
2025-03-22 11:57:06 -0300

    mensagem

---
+++ Main2.1.py
@@ -0,0 +1,18 @@
+soma = 2+2
+subtração = 2-2
+divisão = 10/2
+divisão_inteiro = 10//2
+multiplicação = 2*2
+potencia = 2**3
+resto = 10 % 3
+print('O resultado é: ', soma)
+print('O resultado é: ', subtração)
+print('O resultado é: ', divisão)
+print('O resultado é: ', divisão_inteiro)
+print('O resultado é: ', multiplicação)
+print('O resultado é: ', potencia)
+print('O resultado é: ', resto)
+
+# Declaração de variáveis; soma, subtração, divisão, multiplicação, potencia
+# = para chamar as operações
+# Print com ('') para colocar a mensagem que aparecerá na tela e por fim, colocando a variável de cada operação nos devidos prints (lugares)
---
+++ Main2.2.py
@@ -0,0 +1,9 @@
+n1 = 10
+n2 = 10
+m = n1 + n2
+print('O resultado é: ',m)
+
+# O n1 é a variável onde guardei o número inteiro 10
+# O n2 é outra variável onde também guardei o número inteiro 10
+# Na variável m eu coloquei a soma das duas variáveis que contiam os dois números
+# Na função print coloquei a string ('') com o texto e coloquei para mostrar a variável m que contia a soma das duas variáveis n1 e n2 
