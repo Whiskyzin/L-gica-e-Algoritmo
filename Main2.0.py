61624ba7316bee67cef4a31d0e16eae3e52d0536
Victor <victor.vj1612@hotmail.com>
2025-03-23 19:33:42 -0300

    mensagem

--- Main2.0.py
+++ Main2.0.py
@@ -1,4 +1,9 @@
-print('a soma de 2+2 é: ', 2+2)
-print('a soma de 2+2 é: ', 2+2)
+# Declaro uma variável, coloco a operação matemática
+# Declaro função print para mostrar na tela e adiciono a variável que contém a operação 
 
-#:)
+c = 10 * 10
+print(c)
+
+print(10 * 10)
+
+# Chamo função print e abro () e coloco a operação matemática dentro e me mostrara o resultado na tela
---
+++ Main2.5.py
@@ -0,0 +1,5 @@
+salário = 2000
+aumento = 7
+print('O seu slário será de: ',salário + ( salário * aumento / 100))
+
+print(2000 + ( 2000 * 7 / 100))
